bf72dcce728dd9517c5542a6ab791d066a689b5d
riteshyaduwanshi439 <riteshthinkdebug@gmail.com>
2026-04-08 09:23:37 +0530

    url

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: '3.25.253.112:5000',
         changeOrigin: true,
       },
     },
